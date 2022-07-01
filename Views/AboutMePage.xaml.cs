f9b76e429709b7346a2e800ff6438e31d929b0e3
LApprentiProgrammeur <108550982+LApprentiProgrammeur@users.noreply.github.com>
2022-07-01 15:52:19 -0400

    Add files via upload

---
+++ Views/AboutMePage.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+
+namespace Notes.Views
+{
+    public partial class AboutMePage : ContentPage
+    {
+        public AboutMePage()
+        {
+            InitializeComponent();
+        }
+    }
+}
